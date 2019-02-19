2dee0b4cbdd9024d2a06ed31c4f7aef47a6d9d72
ornelas17027ms <ornelas17027ms@gmail.com>
2019-02-19 16:31:06 -0600

    hi jesus

---
+++ datos.rb
@@ -0,0 +1,14 @@
+requiere 'net/http'
+requiere 'json'
+
+file = File.read(https://api.datos.gob.mx/v1/gobmx.facts)
+
+class prueba 
+
+	@id = id
+	@slug = slug
+	@organization = organization
+	@url = url
+
+end
+
